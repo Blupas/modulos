400f52ab4949a1e7090d90673e917cac8b137886
unknown <ss.ygabriel2007@gmail.com>
2023-08-31 16:52:42 -0300

    calculadora do tucamaiaShow

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var subtrair = require("./modulos/subtrair");
+var dividir = require("./modulos/dividir");
+var mensagem = require("./modulos/mensagem");
 
-console.log(somar(20,90));
-console.log(media(20,90));
+console.log("A soma de x e y é: " + somar(20,90));
+console.log("A media de x e y é: " + media(20,90));
+console.log("A subtração de x e y é: " + subtrair(20,90));
+console.log("A divisão de x e y é: " + dividir(20,90));
+console.log(mensagem("TucamaiaShow"));
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem = function (nome) {
+    return "Seja bem vindo, "+nome+"!";
+  }; 
+  module.exports = mensagem ;
